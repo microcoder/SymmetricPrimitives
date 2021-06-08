32953473908da0034b9bb6094e68033b670e1a9a
Vladimir Demenev <vademenev@gmail.com>
2021-06-08 12:58:33 +0300

    Added auto calculation optimal amount segments

--- SymmetricCircle.py
+++ SymmetricCircle.py
@@ -2,21 +2,53 @@ import math
 import bpy
 import bmesh
 from bpy.types import Operator
-from bpy.props import FloatProperty, IntProperty
+from bpy.props import FloatProperty, IntProperty, BoolProperty
 
+
+# This is table of contains values of truncation a circle on each 1mm of diameter for each amount subdivision when used modifier Subdivision Surface.
+# keys - amount sudivision, values - value of truncation on 1mm of diameter for this subdivision
+circle_trunc_values = {  '4': 0.333334,  '8': 0.097631, '12': 0.044658, '16': 0.025374, '20': 0.016315,
+                        '24': 0.011358, '28': 0.008357, '32': 0.006405, '36': 0.005064, '40': 0.004104,
+                        '44': 0.003393, '48': 0.002852, '52': 0.00243,  '56': 0.002096, '60': 0.001826,
+                        '64': 0.001605, '68': 0.001422, '72': 0.001268, '76': 0.001139, '80': 0.001028,
+                        '84': 0.000932, '88': 0.000849, '92': 0.000777, '96': 0.000714, '100': 0.000658}
+
+
+def calculate_segments(self):
+    for i in circle_trunc_values.items():
+        if round(self.radius * 2 * 1000 * i[1], 2) <= self.max_truncation * 1000:
+            return int(i[0])
+
+
+def get_radius(self):
+    return self.get('radius', 0.1)  # default value
+
+
+def set_radius(self, value):
+    self['radius'] = value
+
+    if self.auto_segments:
+        self['segments'] = calculate_segments(self)
 
 
 def get_segments(self):
-    return self['segments'] if 'segments' in self.keys() else 32  # default value
+    return self.get('segments', 32)  # default value
 
 
 def set_segments(self, value):
-    
-    # Decrement amount of segments while return_value doesn't is a multiple of 4
-    while value%4 != 0:
-        value -= 1
-    
-    self['segments'] = value
+    if not self.auto_segments:
+        self['segments'] = value // 4 * 4
+
+
+def get_auto_segments(self):
+    return self.get('auto_segments', False)
+
+
+def set_auto_segments(self, value):
+    self['auto_segments'] = value
+
+    if value:
+        self['segments'] = calculate_segments(self)
 
 
 class SymmetricCircle(Operator):
@@ -26,15 +58,29 @@ class SymmetricCircle(Operator):
     bl_label = "Symmetric Circle"            # Display name in the interface
     bl_options = {'REGISTER', 'UNDO'}        # Enable undo for the operator
 
-    radius: FloatProperty(attr='bf_Radius', name='Radius', min=0.0001, soft_min=0, soft_max=100, default=0.1,
-                          description='Radius of the circle', unit='LENGTH')
-
-    segments: IntProperty(name="Segments", min=8, soft_max=100, default=32, step=4, get=get_segments, set=set_segments)
-
+    radius: FloatProperty(name='Radius', description='Radius a circle',
+                          default=0.1, min=0.0001, soft_min=0, soft_max=100, precision=2,
+                          subtype='DISTANCE', # unit='LENGTH'
+                          get=get_radius, set=set_radius
+    )
+    segments: IntProperty(name='Segments', description='Amount segments a circle',
+                          min=4, soft_max=100, default=32, step=4,
+                          get=get_segments, set=set_segments
+    )
+    auto_segments: BoolProperty(name='Auto segments',
+                                description='Auto calculation of optimal amount segments for setted inaccuracy by diameter when used modifier Subdiv',
+                                get=get_auto_segments, set=set_auto_segments
+    )
+    max_truncation: FloatProperty(name='Max truncation',
+                                  description='Maximum allowed truncation a circle diameter when use option Auto segments',
+                                  default=0.0005, soft_min=0.0001, soft_max=10, step=0.001, precision=2,
+                                  subtype='DISTANCE', # unit='CAMERA'
+    )
+    
 
     def execute(self, context):
 
-        # self.report({'INFO'}, 'Example report')
+        # self.report({'WARNING'}, 'Example report')
 
         # NOTE: Находясь в режиме редактирования, Blender обрабатывает копию сетки, которая затем сохраняется как данные объекта (context.object.data)
         #       после выхода из режима редактирования. Это может стать причиной того, что не отображаются сделанные изменения в исходной сетке:
@@ -44,6 +90,8 @@ class SymmetricCircle(Operator):
         # NOTE: Не забыть, что код можно запускать в тестовом редакторе Blender'a, чтобы его не перезапускать каждый раз!!
         #       Однако, код в редакторе Blender работает иначе. Логика создания bmesh.ops.create_circle полностью отличается - иногда вершины индексируются не по порядку!
         
+        # FIXME: При 24 сегментах создается 25 вершин/сегментов вместо 24
+        #        Например, при значении 36 сегментов в UI создается 37 вершин (одна накладывается на другую)
         # FIXME: При запуске Blender в консоли, выпадает ошибка - ModuleNotFoundError: No module named 'add_mesh_SymmetricPrimitives'
         # TODO:  Сделать self-test. Полу-реализация доступна в Blend файле версии. Реализовать через вызов оператора mesh.symmetric_circle_add()
 
@@ -63,8 +111,8 @@ class SymmetricCircle(Operator):
             blender_mesh = context.active_object.data
             bm = bmesh.from_edit_mesh(blender_mesh)
         else:
-            return {'FINISHED'}            # Lets Blender know the operator finished successfully.
-         
+            return {'CANCELLED'}            # Lets Blender know the operator finished successfully.
+        
         
         ###################################################################
         # bmesh.ops.create_circle(bm, cap_ends=False, radius=self.radius, segments=self.segments)['verts']  # returning list of BMVert
@@ -105,6 +153,26 @@ class SymmetricCircle(Operator):
         bm.free
         
         return {'FINISHED'}  # Lets Blender know the operator finished successfully
+
+    def draw(self, context):
+        layout = self.layout
+        col = layout.column()
+        # col.label(text='Custom Interface!')
+        col.separator()
+
+        row = col.row(align=True, heading='Radius')
+        row.prop(self, 'radius', text='')
+
+        row = col.row(align=True, heading='Segments')
+        row.prop(self, 'segments', text='')
+        row.active = True if not self.auto_segments else False
+
+        row = col.row(align=True)
+        row.prop(self, 'auto_segments')
+        row_right = row.row(align=True)
+        row_right.active = self.auto_segments
+        row_right.prop(self, 'max_truncation', text='')
+        col.separator()
 
 
 classes = (
